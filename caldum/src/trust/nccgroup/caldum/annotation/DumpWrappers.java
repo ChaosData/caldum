365e47871b1fdfa4fc4fe40e96572389ac3831a3
Jeff Dileo <jtdileo@gmail.com>
2024-10-12 20:48:12 -0400

    fixup license headers based on commit history dates

--- caldum/src/trust/nccgroup/caldum/annotation/DumpWrappers.java
+++ caldum/src/trust/nccgroup/caldum/annotation/DumpWrappers.java
@@ -1,5 +1,6 @@
 /*
-Copyright 2018 NCC Group
+Copyright 2019 NCC Group
+Copyright 2024 Jeff Dileo
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
